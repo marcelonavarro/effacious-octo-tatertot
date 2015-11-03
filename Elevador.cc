597742372c27a313cf29f6e6b4aae7adc0024002
MARCELO RODRIGUES NAVARRO <marcelo.navarro@acad.pucrs.br>
2015-11-03 21:10:08 -0200

    #2

--- Elevador.cc
+++ Elevador.cc
@@ -1,6 +1,7 @@
 //Programação para engenharia II 2015/2
 //////////////////////////////////////// marcelo.navarro@acad.pucrs.br */
-
+//////////////////////////////////////// julio.leal@acad.pucrs.br */
+//////////////////////////////////////// gabrielgneuwald@gmail.com */
 
 
 # include <iostream>
@@ -8,9 +9,31 @@
 
 using namespace std;
 
+class Elevador {
+
+private:
+    int andar;
+    unsigned int ocupacao;//garante que não teremos um numero negativo de pessoas no elevador
+public:
+    Elevador(void);
+    void subir(int n);
+    void descer(int n);
+    //void mover(int n);
+    void entrar(int n);
+    void sair(int n);
+};
+
+Elevador::Elevador(void)
+{
+    andar = 0;
+    ocupacao = 0;
+}
+
 int main (void)
 {
-   cout<<"teste"<<endl;
+    Elevador Aufzug;
+
+    cout<<"teste"<<endl;
     return 0;
 
 }
